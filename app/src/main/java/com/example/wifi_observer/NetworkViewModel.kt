597fc22c722185fbea34e547749e4937fdbd5917
kyo1941 <kyo1941@outlook.jp>
2026-05-09 13:15:45 +0900

    refactor: 不要なインポートを削除

--- app/src/main/java/com/example/wifi_observer/NetworkViewModel.kt
+++ app/src/main/java/com/example/wifi_observer/NetworkViewModel.kt
@@ -1,11 +1,9 @@
 package com.example.wifi_observer
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 
 sealed interface NetworkStatus {
     data object Wifi: NetworkStatus
